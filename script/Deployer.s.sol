5821e2c8789e4ecee3be61150a0af5080769829e
Joshkovu <joashkuteesa223@gmail.com>
2026-02-10 13:12:09 +0300

    refactor:Implement deployment of contracts

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -7,8 +7,28 @@ import {RebaseToken} from "../src/RebaseToken.sol";
 import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/iRebaseToken.sol";
+import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract TokenAndPoolDeployer is Script {
+    function run() public returns (RebaseToken token, RebaseTokenPool pool) {
+        CCIPLocalSimulatorFork ccipLocalSimulatorFork = new CCIPLocalSimulatorFork();
+        Register.NetworkDetails memory networkDetails = ccipLocalSimulatorFork
+            .getNetworkDetails(block.chainid);
+        vm.startBroadcast();
+        token = new RebaseToken();
+        pool = new RebaseTokenPool(
+            IERC20(address(token)),
+            new address[](0),
+            networkDetails.rmnProxyAddress,
+            networkDetails.routerAddress
+        );
+        token.grantMintAndBurnRole(address(pool));
+        vm.stopBroadcast();
+    }
+}
+
+contract VaultDeployer is Script {
     function run(address _rebaseToken) public returns (Vault vault) {
         vm.startBroadcast();
         vault = new Vault(IRebaseToken(_rebaseToken));
