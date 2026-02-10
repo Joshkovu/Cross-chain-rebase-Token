6390c70aa7c1c28cf1ad519bd28d7eb602f9df7f
Joshkovu <joashkuteesa223@gmail.com>
2026-02-10 13:29:30 +0300

    feat: add script to configure pool and update vault and deployer scripts

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -8,6 +8,9 @@ import {RebaseTokenPool} from "../src/RebaseTokenPool.sol";
 import {Vault} from "../src/Vault.sol";
 import {IRebaseToken} from "../src/interfaces/iRebaseToken.sol";
 import {CCIPLocalSimulatorFork, Register} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
+import {RegistryModuleOwnerCustom} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.sol";
+import {TokenAdminRegistry} from "@ccip/contracts/src/v0.8/ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
+
 import {IERC20} from "@ccip/contracts/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
 
 contract TokenAndPoolDeployer is Script {
@@ -24,6 +27,15 @@ contract TokenAndPoolDeployer is Script {
             networkDetails.routerAddress
         );
         token.grantMintAndBurnRole(address(pool));
+        RegistryModuleOwnerCustom(
+            networkDetails.registryModuleOwnerCustomAddress
+        ).registerAdminViaOwner(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress)
+            .acceptAdminRole(address(token));
+        TokenAdminRegistry(networkDetails.tokenAdminRegistryAddress).setPool(
+            address(token),
+            address(pool)
+        );
         vm.stopBroadcast();
     }
 }
